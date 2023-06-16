c5d11d9290836232774ac08273a8a68e865dd500
Alex <alextetekin@gmail.com>
2023-06-16 06:32:30 +0300

    build: add Makefile, add dependencies to build.gradle, add picocli

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,18 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+
+import java.util.concurrent.Callable;
+
+@CommandLine.Command(name = "gendiff", mixinStandardHelpOptions = true,
+        description = "Compares two configuration files and shows a difference.")
+public class App implements Callable<Object> {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        new CommandLine(new App()).execute(args);
+    }
+
+    @Override
+    public Object call() {
+        return null;
     }
 }
