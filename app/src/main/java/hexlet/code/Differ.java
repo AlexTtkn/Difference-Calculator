b3abbd98d9730c3e90b9f475d20dba6eb341a1a6
Alex <alextetekin@gmail.com>
2023-07-07 11:02:27 +0300

    fix: changed Differ.java

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -7,7 +7,6 @@ import java.nio.file.Path;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
-import java.util.stream.Stream;
 
 
 public class Differ {
@@ -24,7 +23,8 @@ public class Differ {
             String stringOutOfPath2 = getStringOutOfPath(path2);
             Map<String, Object> map1 = new TreeMap<>(Parser.getInfoAsMap(stringOutOfPath1, format1));
             Map<String, Object> map2 = new TreeMap<>(Parser.getInfoAsMap(stringOutOfPath2, format2));
-            return getResultString(map1, map2);
+            List<Map<String, Object>> listOfDifference = GetDifference.mergeMapsToList(map1, map2);
+            return Formatter.determineFormat(listOfDifference, format);
         }
         return null;
     }
@@ -39,39 +39,6 @@ public class Differ {
         for (String line : lines) {
             RESULT_STRING.append(line).append("\n");
         }
-        return RESULT_STRING.toString();
-    }
-
-    static List<String> mergeMapsToList(Map<String, Object> map1, Map<String, Object> map2) {
-        return Stream.concat(map1.entrySet().stream(), map2.entrySet().stream())
-                .distinct()
-                .sorted(Map.Entry.comparingByKey())
-                .map(entry -> addSingToResult(entry, map1, map2))
-                .toList();
-    }
-
-    static String addSingToResult(Map.Entry<String, Object> entry, Map<String, Object> map1, Map<String, Object> map2) {
-        if (map1.containsKey(entry.getKey()) && map2.containsKey(entry.getKey())) {
-            if (map1.get(entry.getKey()).equals(map2.get(entry.getKey()))) {
-                return "  " + entry;
-            }
-        }
-        if (map1.entrySet().contains(entry)) {
-            return "- " + entry;
-        } else if (map2.entrySet().contains(entry)) {
-            return "+ " + entry;
-        }
-        return "  " + entry;
-    }
-
-    static String getResultString(Map<String, Object> map1, Map<String, Object> map2) {
-        RESULT_STRING.delete(0, RESULT_STRING.length());
-        RESULT_STRING.append("{\n");
-        for (String entry : mergeMapsToList(map1, map2)) {
-            String replacer = entry.replace("=", " : ");
-            RESULT_STRING.append(replacer).append("\n");
-        }
-        RESULT_STRING.append("}\n");
         return RESULT_STRING.toString();
     }
 }
