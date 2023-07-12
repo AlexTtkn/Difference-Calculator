6bf987b11a63e355876f82ef5497306be39a91fb
Alex <alextetekin@gmail.com>
2023-07-12 07:25:35 +0300

    fix: changed tests

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -8,11 +8,47 @@ import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 
 class DifferTest {
-    private static final Path RESOURCE_DIRECTORY_1 =
-            Path.of("src/test/resources/testFile1.json").toAbsolutePath().normalize();
-    private static final Path RESOURCE_DIRECTORY_2
-            = Path.of("src/test/resources/testFile2.json").toAbsolutePath().normalize();
-    private static final Path WRONG_PATH = Path.of("wrongPath");
+    private static final String RESOURCE_DIRECTORY_1 =
+            Path.of("src/test/resources/testFile1.json").toAbsolutePath().normalize().toString();
+    private static final String RESOURCE_DIRECTORY_2
+            = Path.of("src/test/resources/testFile2.json").toAbsolutePath().normalize().toString();
+    private static final String RESOURCE_DIRECTORY_3
+            = Path.of("src/test/resources/testFile3.yml").toAbsolutePath().normalize().toString();
+    private static final String RESOURCE_DIRECTORY_4
+            = Path.of("src/test/resources/testFile4.yml").toAbsolutePath().normalize().toString();
+    private static final String WRONG_PATH = Path.of("wrongPath").toString();
+
+    @Test
+    void testGenerateValidJson() throws IOException {
+        String expected = """
+                {
+                 - follow : false
+                   host : hexlet.io
+                 - proxy : 123.234.53.22
+                 - timeout : 50
+                 + timeout : 20
+                 + verbose : true
+                }""";
+
+        String actual = Differ.generate(RESOURCE_DIRECTORY_1, RESOURCE_DIRECTORY_2, "stylish");
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void testGenerateValidYaml() throws IOException {
+        String expected = """
+                {
+                 - follow : false
+                   host : hexlet.io
+                 - proxy : 123.234.53.22
+                 - timeout : 50
+                 + timeout : 20
+                 + verbose : true
+                }""";
+
+        String actual = Differ.generate(RESOURCE_DIRECTORY_3, RESOURCE_DIRECTORY_4, "stylish");
+        Assertions.assertEquals(expected, actual);
+    }
 
     @Test
     void testGenerateInvalidPath() {
@@ -30,7 +66,7 @@ class DifferTest {
                   "follow": false
                 }
                 """;
-        String actual = Differ.getStringOutOfPath(RESOURCE_DIRECTORY_1);
+        String actual = Differ.getStringOutOfPath(Path.of(RESOURCE_DIRECTORY_1));
         Assertions.assertEquals(expected, actual);
     }
 
@@ -44,7 +80,7 @@ class DifferTest {
                   "follow": false
                 }
                 """;
-        String actual = Differ.getStringOutOfPath(RESOURCE_DIRECTORY_2);
+        String actual = Differ.getStringOutOfPath(Path.of(RESOURCE_DIRECTORY_2));
         Assertions.assertNotEquals(expected, actual);
     }
 }
