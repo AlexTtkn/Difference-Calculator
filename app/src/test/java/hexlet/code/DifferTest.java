c6b679cdaeff6414d2af9a095829a30e324ab4f0
Alex <alextetekin@gmail.com>
2023-07-24 14:01:32 +0300

    test: refactored DifferTest

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -1,34 +1,19 @@
 package hexlet.code;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 
 class DifferTest {
-    private static String testFile1 = "testFile1.json";
-    private static String testFile2 = "testFile2.json";
-    private static String testFile3 = "testFile3.yml";
-    private static String testFile4 = "testFile4.yml";
-    private static String testFile5 = "testFile5.json";
-    private static String testFile6 = "testFile6.json";
     private static final String WRONG_PATH = "wrongPath";
-    private static final String ROOT_FOR_FIXTURES = "src/test/resources/fixtures/";
     private static final String ROOT_FOR_TESTFILES = "src/test/resources/";
-
-
-    private static String jsonWithInnerStructureStylish = "expected_json_with_inner_structure_stylish";
-    private static String jsonWithInnerStructurePlain = "expected_json_with_inner_structure_plain";
-    private static String jsonYmlStylish = "expected_json_yml_stylish";
-    private static String jsonYmlPlain = "expected_json_yml_plain";
-    private static String jsonYmlJson = "expected_json_yml_json";
-    private static String stringOutOfPath = "expected_string_out_of_path";
-
+    private static final String ROOT_FOR_FIXTURES = "src/test/resources/fixtures/";
 
     private static String getInfoFromFixture(String path) throws IOException {
         return Files.readString(Paths.get(path)).replaceAll("\\r\\n", "\n");
@@ -38,76 +23,38 @@ class DifferTest {
         return path + fileName;
     }
 
-    @BeforeAll
-    public static void beforeTest() {
-        testFile1 = createPath(ROOT_FOR_TESTFILES, testFile1);
-        testFile2 = createPath(ROOT_FOR_TESTFILES, testFile2);
-        testFile3 = createPath(ROOT_FOR_TESTFILES, testFile3);
-        testFile4 = createPath(ROOT_FOR_TESTFILES, testFile4);
-        testFile5 = createPath(ROOT_FOR_TESTFILES, testFile5);
-        testFile6 = createPath(ROOT_FOR_TESTFILES, testFile6);
+    @ParameterizedTest
+    @CsvSource({
+        "expected_json_with_inner_structure_stylish, testFile5.json, testFile6.json, stylish",
+        "expected_json_yml_stylish, testFile3.yml, testFile4.yml, stylish",
+        "expected_json_with_inner_structure_plain, testFile5.json, testFile6.json, plain",
+        "expected_json_yml_plain, testFile3.yml, testFile4.yml, plain",
+        "expected_json_yml_json, testFile1.json, testFile2.json, json",
+        "expected_json_yml_json, testFile3.yml, testFile4.yml, json",
 
-        jsonWithInnerStructureStylish = createPath(ROOT_FOR_FIXTURES, jsonWithInnerStructureStylish);
-        jsonWithInnerStructurePlain = createPath(ROOT_FOR_FIXTURES, jsonWithInnerStructurePlain);
-        jsonYmlStylish = createPath(ROOT_FOR_FIXTURES, jsonYmlStylish);
-        jsonYmlPlain = createPath(ROOT_FOR_FIXTURES, jsonYmlPlain);
-        jsonYmlJson = createPath(ROOT_FOR_FIXTURES, jsonYmlJson);
-        stringOutOfPath = createPath(ROOT_FOR_FIXTURES, stringOutOfPath);
-    }
+    })
+    void globalTest(String fixture, String testFile1, String testFile2, String format) throws IOException {
+        String path = createPath(ROOT_FOR_FIXTURES, fixture);
+        String firstTestFile = createPath(ROOT_FOR_TESTFILES, testFile1);
+        String secondTestFile = createPath(ROOT_FOR_TESTFILES, testFile2);
 
-    @Test
-    void testGenerateValidJson() throws IOException {
-        String expected = getInfoFromFixture(jsonYmlStylish);
-        String actual = Differ.generate(testFile1, testFile2);
+        String expected = getInfoFromFixture(path);
+        String actual = Differ.generate(firstTestFile, secondTestFile, format);
         Assertions.assertEquals(expected, actual);
     }
 
-    @Test
-    void testGenerateValidYml() throws IOException {
-        String expected = getInfoFromFixture(jsonYmlStylish);
-        String actual = Differ.generate(testFile3, testFile4);
-        Assertions.assertEquals(expected, actual);
-    }
+    @ParameterizedTest
+    @CsvSource({
+        "expected_json_yml_stylish, testFile1.json, testFile2.json",
+        "expected_json_yml_stylish, testFile3.yml, testFile4.yml"
+    })
+    void globalTest2(String fixture, String testFile1, String testFile2) throws IOException {
+        String path = createPath(ROOT_FOR_FIXTURES, fixture);
+        String firstTestFile = createPath(ROOT_FOR_TESTFILES, testFile1);
+        String secondTestFile = createPath(ROOT_FOR_TESTFILES, testFile2);
 
-    @Test
-    void testGenerateValidJsonStylish() throws IOException {
-        String expected = getInfoFromFixture(jsonWithInnerStructureStylish);
-        String actual = Differ.generate(testFile5, testFile6, "stylish");
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    void testGenerateValidYmlStylish() throws IOException {
-        String expected = getInfoFromFixture(jsonYmlStylish);
-        String actual = Differ.generate(testFile3, testFile4, "stylish");
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    void testGenerateValidJsonPlain() throws IOException {
-        String expected = getInfoFromFixture(jsonWithInnerStructurePlain);
-        String actual = Differ.generate(testFile5, testFile6, "plain");
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    void testGenerateValidYmlPlain() throws IOException {
-        String expected = getInfoFromFixture(jsonYmlPlain);
-        String actual = Differ.generate(testFile3, testFile4, "plain");
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    void testGenerateValidJsonJson() throws IOException {
-        String expected = getInfoFromFixture(jsonYmlJson);
-        String actual = Differ.generate(testFile1, testFile2, "json");
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    void testGenerateValidYmlJson() throws IOException {
-        String expected = getInfoFromFixture(jsonYmlJson);
-        String actual = Differ.generate(testFile3, testFile4, "json");
+        String expected = getInfoFromFixture(path);
+        String actual = Differ.generate(firstTestFile, secondTestFile);
         Assertions.assertEquals(expected, actual);
     }
 
@@ -115,19 +62,5 @@ class DifferTest {
     void testGenerateInvalidPath() {
         Assertions.assertThrows(NoSuchFileException.class, () ->
                 Differ.generate(WRONG_PATH, WRONG_PATH, "stylish"));
-    }
-
-    @Test
-    void testGetStringOutOfPathValid() throws IOException {
-        String expected = getInfoFromFixture(stringOutOfPath);
-        String actual = Differ.getStringOutOfPath(Path.of(testFile1));
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    void testGetStringOutOfPathNotEqualsInfo() throws IOException {
-        String expected = getInfoFromFixture(stringOutOfPath);
-        String actual = Differ.getStringOutOfPath(Path.of(testFile2));
-        Assertions.assertNotEquals(expected, actual);
     }
 }
