697181be2250987d48ab8190f1a58b345b73a12e
Alex <alextetekin@gmail.com>
2023-07-27 07:55:21 +0300

    test: refactored tests

--- app/src/test/java/hexlet/code/FormatterTest.java
+++ app/src/test/java/hexlet/code/FormatterTest.java
@@ -11,17 +11,14 @@ import java.util.Map;
 class FormatterTest {
     private static final String WRONG_FORMAT = "invalid";
     private static final int INT_FOR_TEST = 10;
+
     @Test
-    public void testDetermineFormatWrongFormat() throws IOException {
+    public void testDetermineFormatWrongFormat() {
         List<Map<String, Object>> list = new ArrayList<>();
         Map<String, Object> map1 = Map.of("key1", "value1", "key2", INT_FOR_TEST);
         list.add(map1);
 
-        Assertions.assertThrows(RuntimeException.class, () -> {
-            Formatter.determineFormat(list, WRONG_FORMAT);
-        });
+        Assertions.assertThrows(RuntimeException.class, () ->
+                Formatter.determineFormat(list, WRONG_FORMAT));
     }
-
-
-
 }
