80a184f8a7f5dde165921ac3ce5490266f9ba7ec
Alex <alextetekin@gmail.com>
2023-08-01 13:07:01 +0300

    fix: changed GetDifference

--- app/src/main/java/hexlet/code/GetDifference.java
+++ app/src/main/java/hexlet/code/GetDifference.java
@@ -1,43 +1,37 @@
 package hexlet.code;
 
 import java.util.List;
+import java.util.ArrayList;
 import java.util.Map;
-import java.util.Objects;
+import java.util.HashMap;
 import java.util.TreeMap;
-import java.util.stream.Stream;
+import java.util.Objects;
 
 public class GetDifference {
-    private static Map<String, Object> map1 = new TreeMap<>();
-    private static Map<String, Object> map2 = new TreeMap<>();
+    public static List<Map<String, Object>> mergeMapsToList(Map<String, Object> map1, Map<String, Object> map2) {
+        List<String> keys = mergeKeys(map1, map2);
+        List<Map<String, Object>> result = new ArrayList<>();
 
-    public static List<Map<String, Object>> mergeMapsToList(Map<String, Object> m1, Map<String, Object> m2) {
-        map1 = m1;
-        map2 = m2;
-
-        return Stream.concat(map1.entrySet().stream(), map2.entrySet().stream())
-                .sorted(Map.Entry.comparingByKey())
-                .map(GetDifference::findDifference)
-                .distinct()
-                .toList();
+        for (String key : keys) {
+            result.add(findDifference(key, map1, map2));
+        }
+        return result;
     }
 
-    static Map<String, Object> findDifference(Map.Entry<String, Object> entry) {
+    private static Map<String, Object> findDifference(String key, Map<String, Object> map1, Map<String, Object> map2) {
         String changed = "changed";
         String unchanged = "unchanged";
         String added = "added";
         String deleted = "deleted";
 
-        String key = entry.getKey();
-        Object value = entry.getValue();
-
         Map<String, Object> map = new TreeMap<>();
 
-        if (checkIfKeyHaveInBothMaps(key) && checkIfKeysAreEqual(key)) {
+        if (checkIfKeyHaveInBothMaps(key, map1, map2) && checkIfKeysAreEqual(key, map1, map2)) {
             map.put("key", key);
-            map.put("value", value);
+            map.put("value", map1.get(key));
             map.put("type", unchanged);
             return map;
-        } else if (checkIfKeyHaveInBothMaps(key) && !checkIfKeysAreEqual(key)) {
+        } else if (checkIfKeyHaveInBothMaps(key, map1, map2) && !checkIfKeysAreEqual(key, map1, map2)) {
             map.put("key", key);
             map.put("value1", map1.get(key));
             map.put("value2", map2.get(key));
@@ -56,11 +50,22 @@ public class GetDifference {
         return map;
     }
 
-    public static boolean checkIfKeyHaveInBothMaps(String key) {
+    private static boolean checkIfKeyHaveInBothMaps(String key, Map<String, Object> map1, Map<String, Object> map2) {
         return map1.containsKey(key) && map2.containsKey(key);
     }
 
-    public static boolean checkIfKeysAreEqual(String key) {
+    private static boolean checkIfKeysAreEqual(String key, Map<String, Object> map1, Map<String, Object> map2) {
         return Objects.equals(map1.get(key), map2.get(key));
+    }
+
+    private static List<String> mergeKeys(Map<String, Object> m1, Map<String, Object> m2) {
+        Map<String, Object> keys = new HashMap<>();
+        keys.putAll(m1);
+        keys.putAll(m2);
+
+        return keys.keySet()
+                .stream()
+                .sorted()
+                .toList();
     }
 }
